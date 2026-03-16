e60b0763eb125b15d9c129b210d88ba664f70427
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2026-03-16 20:06:28 +0200

    Refactor Streamlit CSS for better visibility control

--- app.py
+++ app.py
@@ -18,25 +18,26 @@ tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 # Настройки Streamlit
 st.set_page_config(layout="wide", initial_sidebar_state="auto")
+
 st.markdown("""
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, 
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, 
-    .viewerBadge_text__1JaDK, #MainMenu, footer, header { 
-        display: none !important; 
-    }
-    
-    
-    
-    .center {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-        flex-direction: column;
-        margin-top: 0vh;
-    }
+
+/* Скрываем служебные элементы Streamlit */
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+
+/* Центрирование элементов */
+.center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
+    margin-top: 0vh;
+}
+
 </style>
 """, unsafe_allow_html=True)
 #################################
